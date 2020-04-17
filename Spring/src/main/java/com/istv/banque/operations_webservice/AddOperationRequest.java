a5812e24f544a98875f92e1e5f0941d596c4d2b0
Yuuoox <aymeric.vanderkelen@icloud.com>
2020-04-17 10:42:01 +0200

    fix pour tomcat

--- Spring/src/main/java/com/istv/banque/operations_webservice/AddOperationRequest.java
+++ Spring/src/main/java/com/istv/banque/operations_webservice/AddOperationRequest.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2020.04.08 à 03:19:54 PM CEST 
+// Généré le : 2020.04.17 à 10:39:38 AM CEST 
 //
 
 
@@ -106,13 +106,4 @@ public class AddOperationRequest {
         this.amount = value;
     }
 
-
-    @Override
-    public String toString() {
-        return "AddOperationRequest{" +
-                "idAccount=" + idAccount +
-                ", name='" + name + '\'' +
-                ", amount=" + amount +
-                '}';
-    }
 }
