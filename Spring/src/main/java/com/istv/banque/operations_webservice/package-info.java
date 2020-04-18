1996b6c12c3e91f70c5a3d33c2abdda7d7061bb2
Maxime Fleury <maxime.fleury0@gmail.com>
2020-04-18 16:18:55 +0200

    Transaction et extraction

--- Spring/src/main/java/com/istv/banque/operations_webservice/package-info.java
+++ Spring/src/main/java/com/istv/banque/operations_webservice/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2020.04.17 à 10:39:38 AM CEST 
+// Généré le : 2020.04.18 à 04:15:15 PM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://istv.com/banque/operations_webservice", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
